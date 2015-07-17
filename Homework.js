98aaa9047d6ccbcc510fbb7d3c4d6e3b1924f1d1
gedhiac <yearup007@The-Terminator.local>
2015-07-16 20:07:52 -0700

    New homework
    Almost there

---
+++ Homework.js
@@ -0,0 +1,15 @@
+/**
+ * YearUp: Intro to Programming
+ * ----------------------------
+ * Session #2, Puzzle A
+ */
+
+var count = '34.908';
+var countNum = parseFloat(count);
+
+alert(countNum);
+
+alert(countNum);
+
+
+// why is countNum 34 and not 34.908? Can you fix this?
